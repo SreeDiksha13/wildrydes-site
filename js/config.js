c30da967b974e754f6343c5ab9641ac05764a7f6
P Sree Diksha <143192612+SreeDiksha13@users.noreply.github.com>
2025-11-20 13:38:42 +0530

    Update config.js
    Added ids

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_xbz2Xy9bl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '30tuksmt5do6oqvtce7hafi44l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
